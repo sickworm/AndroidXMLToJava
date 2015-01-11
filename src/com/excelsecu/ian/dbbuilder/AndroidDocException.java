7072c4c16e9e54dcf4c97ce14503d76f400190f2
sickworm <ch.operation@gmail.com>
2015-01-11 12:44:38 +0800

    AndroidDocFilter works(simple page filter)

---
+++ src/com/excelsecu/ian/dbbuilder/AndroidDocException.java
@@ -0,0 +1,20 @@
+package com.excelsecu.ian.dbbuilder;
+
+public class AndroidDocException extends RuntimeException{
+	private static final long serialVersionUID = -611799962850239497L;
+	private int errorCode = NO_ERROR;
+    /**succeed**/
+    public static final int NO_ERROR = 0x00000000;
+    /**Android doc XML format error**/
+    public static final int ATM_FORMAT_ERROR = 0x00000001;
+    /**Android doc 'attribute to method' table format error**/
+    public static final int AXML_FORMAT_ERROR = 0x00000002;
+    
+    public AndroidDocException(int errorCode) {
+        this.errorCode = errorCode;
+    }
+    
+    public int getErrorCode() {
+        return errorCode;
+    }
+}
--- src/com/excelsecu/ian/dbbuilder/AndroidDocFilter.java
+++ src/com/excelsecu/ian/dbbuilder/AndroidDocFilter.java
@@ -9,12 +9,16 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.htmlparser.Node;
 import org.htmlparser.Parser;
 import org.htmlparser.filters.AndFilter;
 import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.OrFilter;
 import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
+import org.htmlparser.util.SimpleNodeIterator;
 
 public class AndroidDocFilter {
 	private static String ENCODE = "UTF8";
@@ -28,7 +32,7 @@ public class AndroidDocFilter {
 		    Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next(); 
 		    String key = (String) entry.getKey(); 
 		    Object value = (String) entry.getValue(); 
-		    System.out.println(key + "\t\t" + value);
+		    System.out.println(key + "\n\t" + value + "\n");
 		}
 	}
 	
@@ -47,10 +51,10 @@ public class AndroidDocFilter {
             	if (start) {
                 	buf = buf.trim();
                 	//remove useless line
-                	if (buf.isEmpty() || buf.equals("\r"))
+                	if (buf.isEmpty())
                 		continue;
                 	//XML Attributes table end, stupid Android doc source HTML don't have </table> here
-            		if (buf.contains("<table")) {
+            		if (buf.contains("<table id=\"constants\"")) {
                     	//System.out.println(content);
             			break;
             		}
@@ -68,16 +72,51 @@ public class AndroidDocFilter {
 	
 	public static HashMap<String, String> filter(String content) {
         try {
+        	HashMap<String, String> attrMap = new HashMap<String, String>();
             Parser parser = Parser.createParser(content, ENCODE);
-            AndFilter filter =   
-                    new AndFilter(new TagNameFilter("tr"),new HasAttributeFilter("id","lattrs")); 
-            NodeList nodeList = parser.parse(filter);
-            System.out.println("---------------------------------------------------------");
-            //System.out.println(nodeList.asString());
-			return new HashMap<String, String>();
+            AndFilter andFilter1 =   
+                    new AndFilter(new TagNameFilter("tr"),new HasAttributeFilter("class","alt-color api apilevel-"));
+            AndFilter andFilter2 =   
+                    new AndFilter(new TagNameFilter("tr"),new HasAttributeFilter("class","api apilevel-"));
+            OrFilter orFilter = new OrFilter(andFilter1, andFilter2);
+            NodeList tableNodeList = parser.parse(orFilter);
+            NodeIterator tableIt = tableNodeList.elements();
+            while(tableIt.hasMoreNodes()) {
+            	Node trNode = tableIt.nextNode();
+            	NodeList trNodeList = trNode.getChildren();
+            	/**
+            	 * ***** trNodeList example *****
+            	 *	Txt (268[6,37],269[7,0]): \nTag (269[7,0],292[7,23]): td class="jd-linkcol"
+				 *	  Tag (292[7,23],381[7,112]): a href="../../../reference/android/view/View.html...
+				 *	    Txt (381[7,112],412[7,143]): android:accessibilityLiveRegion
+				 *	    End (412[7,143],416[7,147]): /a
+				 *	  End (416[7,147],421[7,152]): /td
+				 *	Txt (421[7,152],422[8,0]): \nTag (422[8,0],445[8,23]): td class="jd-linkcol"
+				 *	  Txt (445[8,23],446[9,0]): \n
+				 *	  Tag (446[9,0],530[9,84]): a href="../../../reference/android/view/View.html#s...
+				 *	    Txt (530[9,84],561[9,115]): setAccessibilityLiveRegion(int)
+				 *	    End (561[9,115],565[9,119]): /a
+				 *	  Txt (565[9,119],566[10,0]): \n
+				 *	  End (566[10,0],571[10,5]): /td
+				 *	Txt (571[10,5],572[11,0]): \nTag (572[11,0],609[11,37]): td class="jd-descrcol" width="100%"
+				 *	  Txt (609[11,37],712[14,0]): \nIndicates to accessibility services whether the...
+				 *	  End (712[14,0],717[14,5]): /td
+				 *	Txt (717[14,5],718[15,0]): \n
+				 * ***** trNodeList example *****
+            	 */
+                if (trNodeList.size() != 7) {
+                	throw new AndroidDocException(AndroidDocException.ATM_FORMAT_ERROR);
+                }
+                String attr = trNodeList.elementAt(1).toPlainTextString();
+                attr = attr.replace("\n", "");
+                String method = trNodeList.elementAt(3).toPlainTextString();
+                method = method.replace("\n", "");
+                attrMap.put(attr, method);
+                //System.out.println(attr + " " + method);
+            }
+			return attrMap;
 		} catch (ParserException e) {
-			e.printStackTrace();
-			return new HashMap<String, String>();
+        	throw new AndroidDocException(AndroidDocException.AXML_FORMAT_ERROR);
 		}
 	}
 }
